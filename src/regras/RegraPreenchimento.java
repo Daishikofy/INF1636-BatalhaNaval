86b0a96281c4bb1ed0b89eacc2b62c0b29a60e8d
Daishikofy <daishi.setsuko@gmail.com>
2019-11-15 21:59:05 -0300

    Adicao de funcao de verificacao e insercao de peca (NAO TESTEI AINDA)

--- src/regras/RegraPreenchimento.java
+++ src/regras/RegraPreenchimento.java
@@ -1,8 +1,10 @@
 package regras;
 
 public class RegraPreenchimento  extends RegraGeral {
-	int a[][];
-	Peca[] pecas = new Peca[15];
+	
+	TabuleiroData tabuleiroPrenchendo; //Contem as peças atualmente fixas no tabuleiro
+	TabuleiroData tabuleiroDesenhado; //Contem o tabuleiro como ele deve ser desenhado
+	Peca[] pecas = new Peca[15]; //Possui as peças a serem posicionadas
 	
 	public RegraPreenchimento()
 	{
@@ -13,8 +15,84 @@ public class RegraPreenchimento  extends RegraGeral {
 		
 	}
 	
-	public void validaInsercao(Peca peca)
-	{}
+	public boolean validaInsercao(Peca peca, int x, int y)
+	{
+		int xIni, xFim, yIni, yFim;
+		xIni = x - 1;
+		xFim = x + peca.largura + 1;
+		yIni = y - 1;
+		yFim = y + peca.altura + 1;
+		if (peca.getNome() != "submarinos")
+		{			
+			for (int i = xIni; i <= xFim; i++)
+			{
+				for (int j = yIni; j <= yFim; j++)
+				{
+					if(!tabuleiroPrenchendo.isEmpty(i, j))
+						return false;
+				}
+			}
+			return true;
+		}
+		else
+		{		
+			int xIgnore = -1, yIgnore = -1;
+			
+			if(peca.largura > peca.altura)
+			{
+				if (peca.getComponentes()[0] == '0')	// 0 X 0
+					yIgnore = yIni;						// X 0 X
+				else
+					yIgnore = yFim;     				// X 0 X
+			}											// 0 X 0
+			else
+			{
+				if (peca.getComponentes()[0] == '0')	// 0 X 
+					xIgnore = xIni;						// X 0
+														// 0 X
+				else	
+					xIgnore = xFim;    					// X 0 
+														// 0 X 
+			}											// X 0
+			
+			if (xIgnore > 0)
+				for (int i = xIni; i <= xFim; i++)
+				{
+					for (int j = yIni; j <= yFim; j++)
+					{
+						if (i != xIgnore || (j != yFim && j != yIni))
+							if(!tabuleiroPrenchendo.isEmpty(i, j))
+								return false;
+					}
+				}
+			
+			else if (yIgnore > 0)
+				for (int i = xIni; i <= xFim; i++)
+				{
+					for (int j = yIni; j <= yFim; j++)
+					{
+						if (j != yIgnore || (i != xFim && i != xIni))
+						if(!tabuleiroPrenchendo.isEmpty(i, j))
+							return false;
+					}
+				}
+				
+			return true;
+		}
+	}
+	
+	public void inserePeca (Peca peca, int x, int y)
+	{
+		char[] components = peca.getComponentes();
+		int currentComp = 0;
+		for (int i = x, cX = 0; i < x + peca.largura; i++)
+			for (int j = y, cY = 0; j < y + peca.altura; j++)			
+			{
+				currentComp = x + j * peca.largura;
+				tabuleiroPrenchendo.setCell(components[currentComp], i, j);				
+			}
+		
+	}
 	
 	private void geraPecas()
 	{
