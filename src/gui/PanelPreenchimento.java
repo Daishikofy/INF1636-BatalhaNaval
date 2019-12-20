3e94ba9c634c4c449197d1984d446da6d84c0c95
Daishikofy <daishi.setsuko@gmail.com>
2019-12-19 22:58:54 -0300

    Label do jogador corrente e botao para continuar

--- src/gui/PanelPreenchimento.java
+++ src/gui/PanelPreenchimento.java
@@ -1,10 +1,51 @@
 package gui;
 import javax.swing.*;
 
-public class PanelPreenchimento extends JPanel {
+import interfaces.IObservador;
+import regras.GerenciadorDePreenchimento;
+
+public class PanelPreenchimento extends JPanel implements IObservador{
+	
+	JButton botaoContinuar;
+	JLabel jogadorCorrente;
+	
 	public PanelPreenchimento() {
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		
+		botaoContinuar = new JButton();
+		jogadorCorrente = new JLabel("NOME DO JOGADOR");
+		botaoContinuar.setText("Finalizar");
+		botaoContinuar.setEnabled(false);
 		add(new GridPecas());
 		add(new GridTabuleiro());
+		add(botaoContinuar);
+		add(jogadorCorrente);
+		
+		GerenciadorDePreenchimento.getManager().getRegra().updateUI.cadastrar((IObservador)this);
 	}
+
+	private void podeFinalizar (Boolean value)
+	{		
+		botaoContinuar.setEnabled(value);
+	}
+	
+	private void atualizaJogadorCorrente (String nome)
+	{
+		jogadorCorrente.setText(nome);
+	}
+
+	@Override
+	public void update() {
+		var regra = GerenciadorDePreenchimento.getManager().getRegra();
+		int vez = regra.getVez();
+		//TODO : Implementar o nome para a vez
+		//DEBUG : Abaixo so pra teste
+		if (vez == 1)
+			atualizaJogadorCorrente("Julieta");
+		else
+			atualizaJogadorCorrente("Marcela");
+		
+		podeFinalizar(regra.podeFinalizar());
+	}
+	
 }
