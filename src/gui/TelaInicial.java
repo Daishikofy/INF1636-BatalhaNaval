bbeeb38c44c380eef497897128562c425ac24301
Daishikofy <daishi.setsuko@gmail.com>
2020-01-01 17:41:19 -0300

    Layout da tela inicial

--- src/gui/TelaInicial.java
+++ src/gui/TelaInicial.java
@@ -1,10 +1,14 @@
 package gui;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import regras.RegraJogo;
 
@@ -15,15 +19,41 @@ public class TelaInicial extends JPanel implements ActionListener{
 	
 	public TelaInicial() {
 		
+		setBorder(new EmptyBorder(10, 10, 10, 10));
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
+        
+        JLabel titulo = new JLabel("Batalha Naval");
+        titulo.setFont (titulo.getFont ().deriveFont (64.0f));
+        titulo.setForeground(Color.GRAY.darker());
+        
+        add(titulo, gbc);
+        
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        JPanel buttons = new JPanel(new GridBagLayout());
+        
+        novoJogo = new JButton("Novo Jogo");
+        novoJogo.addActionListener(this);
+        
+		carregarJogo = new JButton("Carregar ultimo jogo");		
+		carregarJogo.addActionListener(this);		
+        
+        buttons.add(novoJogo, gbc);
+        buttons.add(new JLabel(" "),gbc);
+        buttons.add(carregarJogo, gbc);
+
+        gbc.weighty = 1;
+        add(buttons, gbc);
+		
+		/*
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
-		novoJogo = new JButton("Novo Jogo");
-		carregarJogo = new JButton("Carregar ultimo jogo");
-		novoJogo.addActionListener(this);
-		carregarJogo.addActionListener(this);
-		
-		add(novoJogo);
-		add(carregarJogo);
+		*/
 	}
 
 	@Override
